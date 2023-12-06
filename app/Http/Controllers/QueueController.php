80cf685689cdfa61c1afdcdbcdbba6ebd380aabb
Lizmolina <lizmolinadmt@gmail.com>
2023-12-06 13:36:26 -0600

    Controlador de Colas terminado

--- app/Http/Controllers/QueueController.php
+++ app/Http/Controllers/QueueController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Queue;
 use Illuminate\Http\Request;
 
 class QueueController extends Controller
 {
-    //
+    public function index()
+	{
+		return view('queue.show', [
+		'cola' => Queue::where(function ($query) {
+			$query->where('user_id', request()->user()->id);
+		})->paginate(30)
+		]);
+	}
 }
