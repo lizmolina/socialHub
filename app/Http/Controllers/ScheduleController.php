c0b22d0fd01c1d92ebb87a5c95a4aa2511c2fc70
Lizmolina <lizmolinadmt@gmail.com>
2023-12-06 13:38:09 -0600

    Controlador Calendario listo

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -3,8 +3,61 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Schedule;
 
 class ScheduleController extends Controller
 {
-    //
+    public function index()
+	{
+		return view('schedule.show', [
+		'schedules' => Schedule::where(function ($query) {
+			$query->where('user_id', request()->user()->id);
+		})->paginate(50)
+		]);
+	}
+
+	public function create()
+	{
+		return view('schedule.create');
+	}
+
+    public function edit($id)
+    {
+        $schedule = Schedule::find($id);
+        return view('schedule.edit')->with('schedule', $schedule);
+    }
+
+    public function store(Request $request)
+	{
+        $schedule = new Schedule();
+
+        $schedule->date = $request->get('date');
+        $schedule->time = $request->get('time');
+		$schedule->user_id = request()->user()->id;
+        $schedule->status = 'true';
+
+        $schedule->save();
+
+		return redirect('schedule');
+	}
+
+    public function update(Request $request, $id)
+	{
+        $schedule = Schedule::find($id);
+
+        $schedule->date = $request->get('date');
+        $schedule->time = $request->get('time');
+
+        $schedule->save();
+
+		return redirect('schedule');
+	}
+
+    public function destroy($id)
+    {
+        $schedule = Schedule::find($id);
+        $schedule->delete();
+
+        return redirect('schedule');
+    }
 }
