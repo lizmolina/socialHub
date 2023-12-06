d32159ce5cb9f68c88e63d9a6f1e0626d5ebb836
Lizmolina <lizmolinadmt@gmail.com>
2023-12-06 13:01:31 -0600

    LoginTwitterController Codigo con comentarios

--- app/Http/Controllers/LoginTwitterController.php
+++ app/Http/Controllers/LoginTwitterController.php
@@ -3,57 +3,88 @@
 namespace App\Http\Controllers;
 
 use Abraham\TwitterOAuth\TwitterOAuth;
-
+use Illuminate\Http\Request;
 use App\Models\User;
 
-use Illuminate\Http\Request;
+/**
+ * Controlador para la autenticación y la interacción con la API de Twitter.
+ */
 class LoginTwitterController extends Controller
 {
+    private $twitter_api_key;
+    private $twitter_api_key_secret;
 
-  private $twitter_api_key;
-  private $twitter_api_key_secret;
+    /**
+     * Constructor del controlador.
+     * Inicializa las claves de la API de Twitter con valores de las variables de entorno.
+     */
+    public function __construct()
+    {
+        $this->twitter_api_key_secret = env('TWITTER_SECRET');
+        $this->twitter_api_key = env('TWITTER_KEY');
+    }
 
-  public function __construct()
-  {
-    $this->twitter_api_key_secret = env('TWITTER_SECRET');
-    $this->twitter_api_key = env('TWITTER_KEY');
-  }
-
+    /**
+     * Inicia el proceso de autenticación con Twitter.
+     * Redirige al usuario a la página de autorización de Twitter.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function loginTwitter(Request $request)
     {
-        $callback=route('k');
-        $_twitter_connect = new TwitterOAuth($this->twitter_api_key,$this->twitter_api_key_secret);
+        $callback = route('k'); // Modificar con el nombre de la ruta de callback apropiada
+        $_twitter_connect = new TwitterOAuth($this->twitter_api_key, $this->twitter_api_key_secret);
         $_twitter_connect->setApiVersion('2');
-        $access_token =  $_twitter_connect->oauth("oauth/request_token", ["oauth_callback" => $callback]);
-        $route = $_twitter_connect->url('oauth/authorize',['oauth_token'=> $access_token['oauth_token']]);
+        $access_token = $_twitter_connect->oauth("oauth/request_token", ["oauth_callback" => $callback]);
+        $route = $_twitter_connect->url('oauth/authorize', ['oauth_token' => $access_token['oauth_token']]);
         return redirect($route);
     }
 
+    /**
+     * Maneja la respuesta de Twitter después de la autenticación.
+     * Obtiene y almacena el token de acceso de Twitter.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function getToken(Request $request)
     {
-        $response= $request->all();
+        $response = $request->all();
 
-        $oauth_token= $response['oauth_token'];
-        $oauth_verifer=  $response['oauth_verifier'];
-        $_twitter_connect = new TwitterOAuth($this->twitter_api_key,$this->twitter_api_key_secret,$oauth_token,$oauth_verifer);
+        $oauth_token = $response['oauth_token'];
+        $oauth_verifier = $response['oauth_verifier'];
+        $_twitter_connect = new TwitterOAuth($this->twitter_api_key, $this->twitter_api_key_secret, $oauth_token, $oauth_verifier);
         $_twitter_connect->setApiVersion('2');
-        $token =  $_twitter_connect->oauth('oauth/access_token',['oauth_verifier'=> $oauth_verifer]);
-        $oauth_toke=$token['oauth_token'];
-        $oauth_token_secret= $token['oauth_token_secret'];
+        $token = $_twitter_connect->oauth('oauth/access_token', ['oauth_verifier' => $oauth_verifier]);
+        $oauth_token = $token['oauth_token'];
+        $oauth_token_secret = $token['oauth_token_secret'];
+
+        // Aquí asumo que el usuario está autenticado y se actualiza su registro en la base de datos.
+        // Deberías adaptar esta parte según la lógica de tu aplicación.
         $user = User::query()->update(
-            ['twitter_oauth_token' => $oauth_toke,
-            'twitter_oauth_token_secret' => $oauth_token_secret,
+            [
+                'twitter_oauth_token' => $oauth_token,
+                'twitter_oauth_token_secret' => $oauth_token_secret,
             ]
         );
-        return redirect('dashboard-user');
-  }
 
-    public function postTweet($oauth_token,$oauth_token_secret,$post){
-        $push= new TwitterOAuth($this->twitter_api_key,$this->twitter_api_key_secret,$oauth_token,$oauth_token_secret);
-        $push->setApiVersion('2');
-        $response = $push->post('tweets', ['text' => $post], true);
-       // $push->setTimeouts(10, 15);
-        //return redirect()->route('we');
+        return redirect('dashboard-user'); // Redirige al dashboard del usuario.
     }
 
+    /**
+     * Publica un tweet en la cuenta del usuario autenticado.
+     *
+     * @param string $oauth_token Token de OAuth del usuario.
+     * @param string $oauth_token_secret Secreto del token de OAuth del usuario.
+     * @param string $post Texto del tweet a publicar.
+     */
+    public function postTweet($oauth_token, $oauth_token_secret, $post)
+    {
+        $push = new TwitterOAuth($this->twitter_api_key, $this->twitter_api_key_secret, $oauth_token, $oauth_token_secret);
+        $push->setApiVersion('2');
+        $response = $push->post('tweets', ['text' => $post], true);
+
+        // Aquí podrías manejar la respuesta de la API de Twitter, por ejemplo, verificar si el tweet se publicó correctamente.
+    }
 }
